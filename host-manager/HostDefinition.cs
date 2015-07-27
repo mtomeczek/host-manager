e65e46caa8e995a4a4008f71c9df1535e61c34a3
Michal Tomeczek <mtomeczek74@gmail.com>
2015-07-28 00:43:38 +0200

    Added host definition. Added host listing.

---
+++ host-manager/HostDefinition.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.IO;
+
+namespace hostmanager
+{
+	public class HostDefinition
+	{
+		public IPAddress Address;
+
+		public string Host;
+
+		public HostDefinition (IPAddress address, string host)
+		{
+			this.Address = new IPAddress (address.GetAddressBytes ());
+			this.Host = host.Trim();
+		}
+
+		public static List<HostDefinition> readHosts (string fileName)
+		{
+			List<HostDefinition> hosts = new List<HostDefinition> ();
+			StreamReader hostsFile = File.OpenText (fileName);
+			while (!hostsFile.EndOfStream) {
+				string hostLine = hostsFile.ReadLine ();
+				string hostText = hostLine.Split ('#') [0];
+				string ipText = hostText.Substring (0, hostText.IndexOf (" ")).Trim ();
+				string namesText = hostText.Substring (hostText.IndexOf (" ")).Trim ();
+				foreach(string nameText in namesText.Split(" \t".ToCharArray())){
+					IPAddress hostAddress;
+					if(IPAddress.TryParse(ipText,out hostAddress)){
+						hosts.Add (new HostDefinition (hostAddress, nameText));
+					}
+				}
+
+			}
+			return hosts;
+		}
+	}
+}
+
--- host-manager/MainWindow.cs
+++ host-manager/MainWindow.cs
@@ -1,16 +1,88 @@
 ﻿using System;
+using System.Windows;
 using Gtk;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using hostmanager;
+using GLib;
+using System.Collections.Generic;
 
 public partial class MainWindow: Gtk.Window
 {
+	
 	public MainWindow () : base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
+		initHosts ();
 	}
 
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
 	{
-		Application.Quit ();
+		Gtk.Application.Quit ();
 		a.RetVal = true;
+	}
+
+	protected void OnOpenActionActivated (object sender, EventArgs e)
+	{		
+		FileChooserDialog fileDialog = new FileChooserDialog ("Open file", null, FileChooserAction.Open,"Cancel", ResponseType.Cancel, "Select",ResponseType.Accept);
+		fileDialog.ShowHidden = true;
+		fileDialog.SetCurrentFolder("c:\\windows\\system32\\drivers\\etc\\");
+
+		try{
+			ResponseType result = (ResponseType)fileDialog.Run ();
+			if(result == ResponseType.Accept){
+				fillHosts(fileDialog.Filename);
+			}
+			else{
+				
+			}
+			fileDialog.Destroy();				
+		}
+		catch(Exception ex)
+		{
+			
+		}
+	}
+		
+	private void initHosts(){
+		TreeViewColumn colIp = new TreeViewColumn ();
+		colIp.Title = "IP";
+		CellRendererText colIpRenderer = new CellRendererText ();
+		colIp.PackStart (colIpRenderer, true);
+		colIp.AddAttribute (colIpRenderer, "Address", 0);
+		colIp.SetCellDataFunc (colIpRenderer, new TreeCellDataFunc (renderHostIp));
+
+		TreeViewColumn colHost = new TreeViewColumn ();
+		colHost.Title = "Host";
+		CellRendererText colHostRenderer = new CellRendererText ();
+		colHost.PackStart (colHostRenderer, true);
+		colHost.AddAttribute (colHostRenderer, "Host", 1);
+		colHost.SetCellDataFunc (colHostRenderer, new TreeCellDataFunc (renderHostName));
+		grdHosts.AppendColumn (colIp);
+		grdHosts.AppendColumn (colHost);
+	}
+
+	private void fillHosts(string hostsFileName){
+		List<HostDefinition> hosts= HostDefinition.readHosts (hostsFileName);
+		ListStore hostsStore = new ListStore (typeof(HostDefinition));
+
+		grdHosts.Model = hostsStore;
+		foreach(HostDefinition host in hosts){
+			hostsStore.AppendValues (host);
+		}
+			
+	}
+
+	private void renderHostIp (TreeViewColumn column, CellRenderer cell, TreeModel model, TreeIter iter)
+	{
+		HostDefinition host = (HostDefinition) model.GetValue (iter, 0);
+		(cell as Gtk.CellRendererText).Text = host.Address.ToString();
+	}
+
+	private void renderHostName (Gtk.TreeViewColumn column, Gtk.CellRenderer cell, Gtk.TreeModel model, Gtk.TreeIter iter)
+	{
+		HostDefinition host = (HostDefinition) model.GetValue (iter, 0);
+		(cell as Gtk.CellRendererText).Text = host.Host;
 	}
 }
